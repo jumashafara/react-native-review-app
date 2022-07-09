9308280e35ed69df1fb3806259994b86b117ae81
jumashafara <jumashafara0@gmail.com>
2022-07-09 21:51:47 +0300

    initailized project

---
+++ screens/Reviewdetatils.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ReviewDetails = () => {
+  return (
+    <View>
+      <Text>ReviewDetails</Text>
+    </View>
+  );
+};
+
+export default ReviewDetails;
